e170d35fcbe503dc302748b6f94dc7bc4d5a8cc9
ともあき <45200082+tomoaki33@users.noreply.github.com>
2021-11-19 00:35:06 +0900

    Update common.js

--- script/common.js
+++ script/common.js
@@ -284,3 +284,9 @@ for (let i = 0; i < 6; i++) {
 		menu.classList.toggle('sp-nav');
 	},false);
 }
+document.addEventListener('click', (e) => {
+	if (!e.target.closet(menu)) {
+     		ham.classList.toggle('toggle');
+		menu.classList.toggle('sp-nav');
+	}
+},false);
