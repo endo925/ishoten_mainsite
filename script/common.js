9d66f67f1fe724cf5a20c6dfef23afe782f645d3
ともあき <45200082+tomoaki33@users.noreply.github.com>
2021-11-19 00:58:56 +0900

    Update common.js

--- script/common.js
+++ script/common.js
@@ -274,12 +274,6 @@ window.addEventListener('scroll', ()=>{
 var ham = document.getElementsByClassName('ham')[0];
 var menu = document.getElementsByClassName('pc-nav')[0];
 var jump = document.getElementsByClassName('jump');
-document.addEventListener('click', (e) => {
-	if (document.getElementsByClassName('toggle')[0] != null && !e.target.closest('.sp-nav')) {
-     		ham.classList.toggle('toggle');
-		menu.classList.toggle('sp-nav');
-	}
-},false);
 ham.addEventListener('click', () => {
      	ham.classList.toggle('toggle');
 	menu.classList.toggle('sp-nav');
